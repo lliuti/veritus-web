a37454b00d10948da5567571627333358042c33b
Lucas Liuti <53618919+lliuti@users.noreply.github.com>
2022-09-26 14:05:10 -0300

    Update AddItemDialog.jsx

--- src/components/inventory/AddItemDialog.jsx
+++ src/components/inventory/AddItemDialog.jsx
@@ -166,6 +166,11 @@ export const AddItemDialog = (props) => {
                                 <MenuItem value="3">3</MenuItem>
                                 <MenuItem value="4">4</MenuItem>
                                 <MenuItem value="5">5</MenuItem>
+                                <MenuItem value="6">6</MenuItem>
+                                <MenuItem value="7">7</MenuItem>
+                                <MenuItem value="8">8</MenuItem>
+                                <MenuItem value="9">9</MenuItem>
+                                <MenuItem value="10">10</MenuItem>
                             </Select>
                         </FormControl>
                     </Grid>
