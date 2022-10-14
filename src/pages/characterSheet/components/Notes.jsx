2951355db626c7676b39255f210e7bd21c6ddf7a
Lucas Liuti <53618919+lliuti@users.noreply.github.com>
2022-10-14 18:50:20 -0300

    Update Notes.jsx

--- src/pages/characterSheet/components/Notes.jsx
+++ src/pages/characterSheet/components/Notes.jsx
@@ -36,7 +36,7 @@ export function Notes({ characterNotes, fetchCharacter }) {
     }
 
     return (
-        <Grid item xs={12} md={6} sx={{ mt: 2 }}>
+        <Grid item xs={12} md={6} sx={{ mt: 4 }}>
             <Grid container spacing={1}>
                 <Grid item xs={12} sm={6}>
                     <Typography component="h1" variant="h5" color="inherit" sx={{ mb: 1}}>Resistencias</Typography>
