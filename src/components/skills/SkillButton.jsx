93146d9739b14f9ea1b68707e2e158afe1684172
Lucas Liuti <53618919+lliuti@users.noreply.github.com>
2022-10-14 11:33:36 -0300

    Create SkillButton.jsx

---
+++ src/components/skills/SkillButton.jsx
@@ -0,0 +1,19 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export const SkillButton = ({ skillShort, skillFull, skill, handleSkillRollOpen, skillRollEnabled }) => {
+    return (
+        <Button 
+            onClick={handleSkillRollOpen} 
+            disabled={skillRollEnabled}
+            color="secondary" 
+            variant='outlined' 
+            fullWidth
+            size="small"
+            sx={{ my: 0.4, px: 2, display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+                <Typography component="span" variant="inherit">{skillShort}</Typography>
+                <Typography component="span" variant="inherit">{skillFull}</Typography>
+                <Typography component="span" variant="inherit">{skill != "0" ? `+${skill}` : "0"}</Typography>
+        </Button>
+    )
+}
