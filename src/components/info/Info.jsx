c99a08a02c470e1c6077d168cf3a5991f5860124
Lucas Liuti <53618919+lliuti@users.noreply.github.com>
2022-10-15 23:54:57 -0300

    Update Info.jsx

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -5,6 +5,12 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+
+import Dialog from '@mui/material/Dialog';
+
 export const Info = ({ characterInfo }) => {
     const [name, setName] = useState('');
     const [age, setAge] = useState(0);
@@ -16,6 +22,8 @@ export const Info = ({ characterInfo }) => {
     const [greatestQuality, setGreatestQuality] = useState('');
     const [worstFlaw, setWorstFlaw] = useState('');
     const [belief, setBelief] = useState('');
+    const [pictureUrl, setPictureUrl] = useState('');
+    const [updatePictureDialog, setUpdatePictureDialog] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
 
     useEffect(() => {
@@ -29,6 +37,7 @@ export const Info = ({ characterInfo }) => {
         setGreatestQuality(characterInfo.greatestQuality);
         setWorstFlaw(characterInfo.worstFlaw);
         setBelief(characterInfo.belief);
+        setPictureUrl(characterInfo.pictureUrl);
     }, [characterInfo]);
 
     const handleUpdateCharacterInfo = async () => {
@@ -57,167 +66,231 @@ export const Info = ({ characterInfo }) => {
 
     return (
         <Grid item xs={12} sm={6} md={12} sx={{ mt: 5 }}>
-            <Typography component="h1" variant="h5" color="inherit">Características</Typography>
-            <Grid container columnSpacing={1}>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="name"
-                        label="Nome"
-                        name="name"
-                        fullWidth
-                        variant="filled"
-                        color='secondary'
-                        value={name || ''}
-                        onChange={(event) => setName(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
+            <Typography component="h1" variant="h6" color="inherit" sx={{ mb: 1 }}>Características</Typography>
+            <Grid container spacing={1}>
+                <Grid onClick={() => setUpdatePictureDialog(true)} item xs={12} md={1} display={{ xs: "flex", sm: "none" }}>
+                    <Card sx={{ borderRadius: "0%", mt: 1 }}>
+                        <CardMedia
+                            component="img"
+                            image={pictureUrl}
+                            alt="Character picture"
+                        />
+                    </Card>
                 </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="nickname"
-                        label="Apelido(s)"
-                        name="nickname"
-                        fullWidth
-                        variant="filled"
-                        color='secondary'
-                        value={nickname || ''}
-                        onChange={(event) => setNickname(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
+                <Grid onClick={() => setUpdatePictureDialog(true)} item xs={12} md={1} display={{ xs: "none", sm: "flex" }}>
+                    <img src={pictureUrl} alt="Character picture" width="104px" />
                 </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="age"
-                        label="Idade"
-                        name="age"
-                        fullWidth
-                        variant="filled"
-                        type="number"
-                        color='secondary'
-                        value={age || 0}
-                        onChange={(event) => setAge(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="height"
-                        label="Altura"
-                        name="height"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={height || ''}
-                        onChange={(event) => setHeight(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="body-type"
-                        label="Porte Físico"
-                        name="bodyType"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={bodyType || ''}
-                        onChange={(event) => setBodyType(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="hair"
-                        label="Cabelo"
-                        name="hair"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={hair || ''}
-                        onChange={(event) => setHair(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="gender"
-                        label="Genero"
-                        name="gender"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={gender || ''}
-                        onChange={(event) => setGender(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="belief"
-                        label="Crença"
-                        name="belief"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={belief || ''}
-                        onChange={(event) => setBelief(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="greatest-quality"
-                        label="Maior Qualidade"
-                        name="hair"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={greatestQuality || ''}
-                        onChange={(event) => setGreatestQuality(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
-                </Grid>
-                <Grid item xs={6} sm={6} md={2.4}>
-                    <TextField
-                        size="small"
-                        margin="normal"
-                        id="worst-flaw"
-                        label="Pior Defeito"
-                        name="worst-flaw"
-                        fullWidth
-                        variant="filled"
-                        type="text"
-                        color='secondary'
-                        value={worstFlaw || ''}
-                        onChange={(event) => setWorstFlaw(event.target.value)}
-                        onBlur={handleUpdateCharacterInfo}
-                    />
+                <Grid item xs={12} md={11}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="name"
+                                label="Nome"
+                                name="name"
+                                fullWidth
+                                variant="filled"
+                                color='secondary'
+                                value={name || ''}
+                                onChange={(event) => setName(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="nickname"
+                                label="Apelido(s)"
+                                name="nickname"
+                                fullWidth
+                                variant="filled"
+                                color='secondary'
+                                value={nickname || ''}
+                                onChange={(event) => setNickname(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="age"
+                                label="Idade"
+                                name="age"
+                                fullWidth
+                                variant="filled"
+                                type="number"
+                                color='secondary'
+                                value={age || 0}
+                                onChange={(event) => setAge(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="height"
+                                label="Altura"
+                                name="height"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={height || ''}
+                                onChange={(event) => setHeight(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="body-type"
+                                label="Porte Físico"
+                                name="bodyType"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={bodyType || ''}
+                                onChange={(event) => setBodyType(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="hair"
+                                label="Cabelo"
+                                name="hair"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={hair || ''}
+                                onChange={(event) => setHair(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="gender"
+                                label="Genero"
+                                name="gender"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={gender || ''}
+                                onChange={(event) => setGender(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="belief"
+                                label="Crença"
+                                name="belief"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={belief || ''}
+                                onChange={(event) => setBelief(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="greatest-quality"
+                                label="Maior Qualidade"
+                                name="hair"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={greatestQuality || ''}
+                                onChange={(event) => setGreatestQuality(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                        <Grid item xs={6} sm={6} md={2.4}>
+                            <TextField
+                                size="small"
+                                id="worst-flaw"
+                                label="Pior Defeito"
+                                name="worst-flaw"
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                color='secondary'
+                                value={worstFlaw || ''}
+                                onChange={(event) => setWorstFlaw(event.target.value)}
+                                onBlur={handleUpdateCharacterInfo}
+                            />
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
+            <UpdatePicture 
+                onClose={() => setUpdatePictureDialog(false)} 
+                open={updatePictureDialog} 
+                pictureUrl={pictureUrl} 
+                setPictureUrl={setPictureUrl}
+                enqueueSnackbar={enqueueSnackbar}
+                id={characterInfo.id}
+            />
         </Grid>
+    )
+}
+
+const UpdatePicture = ({ onClose, open, pictureUrl, setPictureUrl, enqueueSnackbar, id }) => {
+    const handleClose = () => {
+        onClose();;
+    };
+
+    const handleUpdatePicture = async () => {
+        try {
+            await api.put(`/characters/${id}/info`, { pictureUrl });
+            enqueueSnackbar("Imagem atualizada.", { 
+                variant: "info"
+            });
+            handleClose();
+        } catch (err) {
+            console.log(err);
+            enqueueSnackbar("Não foi possível atualizar a imagem.", { 
+                variant: "error"
+            });
+        }
+    }
+
+    return (
+        <Dialog onClose={handleClose} open={open} fullWidth maxWidth='sm'>
+            <Grid container sx={{ p: 2}} spacing={1}>
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h6" color="inherit">Imagem</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography component="p" variant="body2" color="text.secondary">Note que alguns formatos de imagem ou servidores de hospedagem podem não ser suportados.</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        size="small"
+                        id="picture"
+                        label="Link da imagem"
+                        name="picture"
+                        fullWidth
+                        variant="filled"
+                        color='secondary'
+                        value={pictureUrl}
+                        onChange={(event) => setPictureUrl(event.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button onClick={handleUpdatePicture} fullWidth color="secondary">Atualizar imagem</Button>
+                </Grid>
+            </Grid>
+        </Dialog>
     )
 }
